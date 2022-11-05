5b6c021265c5cbbb2662274c7f55e5d4511d0c36
Gaspard GOUPY <gaspard.goupy@etu.univ-lyon1.fr>
2022-11-05 19:09:22 +0100

    Adapt baseline w/o trimming

--- TIDIGITS/baseline.py
+++ TIDIGITS/baseline.py
@@ -7,16 +7,19 @@ from utils import load_TIDIGITS
 
 
 
-def main(seed=0):
+def main(seed=0, trim=True):
 
     random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
     np.random.seed(seed)
 
-    X_train, X_test, y_train, y_test = load_TIDIGITS(seed=seed)
-    X_train = X_train.reshape(-1, 55*40)
-    X_test = X_test.reshape(-1, 55*40)
-    
+    if trim:
+        X_train, X_test, y_train, y_test = load_TIDIGITS(seed=seed)
+        X_train = X_train.reshape(-1, 55*40)
+    else:
+        X_train, X_test, y_train, y_test = load_TIDIGITS(seed=seed, trim=False, sample_size=18000)
+        X_test = X_test.reshape(-1, 71*40)
+
     clf = LinearSVC(random_state=seed, max_iter=10000, C=0.005)
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
